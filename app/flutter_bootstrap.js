3a123d7c1da147923e43d24cbcc159da701a4eb0
Jian Lun Li <jaron0211@gmail.com>
2026-03-31 02:22:25 +0800

    fix: parse nav_goal and alert_action in PlanStep for topic display

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "50522563"
+    serviceWorkerVersion: "3644243868"
   }
 });
