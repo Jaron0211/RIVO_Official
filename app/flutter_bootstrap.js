f00104fdd0fe464aa98d45d4b6be3624d067d940
Jian Lun Li <jaron0211@gmail.com>
2026-04-01 13:02:38 +0800

    feat: login page — Chinese support + animated wave background

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2282225912"
+    serviceWorkerVersion: "2517874346"
   }
 });
