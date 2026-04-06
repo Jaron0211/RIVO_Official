395fc52a4ceeb068f9e4490bf41189850d825072
Jian Lun Li <jaron0211@gmail.com>
2026-04-06 16:07:02 +0800

    Phase 1+2: camera collapse, 2D/3D toggle, compact track selector

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2237995257"
+    serviceWorkerVersion: "2417176519"
   }
 });
